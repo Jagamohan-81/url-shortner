e7aeb756c28ba69be70ae0f71552ab194abfd2c3
Jagamohan Panda <pandajagamohan12@gmail.com>
2025-03-15 20:42:06 +0530

    [JP] dummy commit for deployment

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ const swaggerDocument = require("./docs/swagger.json");
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
-
 app.use(express.json());
 app.use(cors());
 app.use(morgan("dev"));
