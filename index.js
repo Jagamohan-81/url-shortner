16657049f834981a109a2a2c4f35a119ef6b15e9
Jagamohan Panda <pandajagamohan12@gmail.com>
2025-03-15 20:01:26 +0530

    [JP] swagger added to production

--- index.js
+++ index.js
@@ -14,10 +14,10 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cors());
 app.use(morgan("dev"));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use("/", urlRoutes);
 app.use("/", statsRoutes);
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
